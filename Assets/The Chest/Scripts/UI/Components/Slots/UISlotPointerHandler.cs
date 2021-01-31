df07b9ced545a1d671a3ecf431b24fccbe20f58a
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2021-01-31 19:01:19 -0300

    Tooltip class stated

---
+++ Assets/The Chest/Scripts/UI/Components/Slots/UISlotPointerHandler.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using TheChest.Scripts.UI.Components;
+
+namespace 
+    TheChest.UI.Components.Slots
+{
+    /// <summary>
+    /// Class to handle Click On Slot
+    /// </summary>
+    [RequireComponent(typeof(UISlot))]
+    [DisallowMultipleComponent]
+    public sealed class UISlotPointerHandler : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler , IPointerExitHandler
+    {
+        [SerializeField] UISlot uiSlot;
+        [SerializeField] UITooltip tooltip;
+
+        public void Start()
+        {
+            this.uiSlot = this.GetComponent<UISlot>();
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button == PointerEventData.InputButton.Left)
+            {
+                uiSlot.Select();
+            }
+            else if (eventData.button == PointerEventData.InputButton.Right)
+            {
+
+            }
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            this.tooltip = Instantiate(tooltip, this.transform.position * 0.1f,transform.rotation);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Destroy(this.tooltip);
+        }
+    }
+}
---
+++ Assets/The Chest/Scripts/UI/Components/UITooltip.cs
@@ -0,0 +1,22 @@
+﻿using TheChest.Items;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TheChest.Scripts.UI.Components
+{
+    public class UITooltip : MonoBehaviour
+    {
+        [SerializeField] Image itemIcon;
+
+        [SerializeField] Text itemName;
+        [SerializeField] Text itemDescription;
+
+        public void ToggleShow(Item item)
+        {
+            this.itemIcon.sprite = item.Image;
+
+            this.itemName.text = item.Name;
+            this.itemDescription.text = item.Description;
+        }
+    }
+}
